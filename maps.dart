7953f692cf781dbf51f5244b07060bcba3cd072f
dongmin7208 <dongmin.park.career@gmail.com>
2023-06-03 17:51:16 +0900

    set

---
+++ maps.dart
@@ -0,0 +1,21 @@
+void main() {
+  var player = {
+    'name': 'dondon',
+    'xp': 19.99,
+    'superpower': false,
+  };
+  Map<int, bool> player2 = {1: true, 2: false, 3: true};
+}
+
+void main2() {
+  List<Map<String, Object>> players = [
+    {'name': 'dondon', 'xp': 19.99, 'superpower': false},
+    {'name': 'suga', 'xp': 19.99, 'superpower': false},
+    {'name': 'jimin', 'xp': 19.99, 'superpower': false},
+    {'name': 'jungkook', 'xp': 19.99, 'superpower': false},
+    {'name': 'jhope', 'xp': 19.99, 'superpower': false},
+    {'name': 'jin', 'xp': 19.99, 'superpower': false},
+    {'name': 'rm', 'xp': 19.99, 'superpower': false},
+    {'name': 'v', 'xp': 19.99, 'superpower': false},
+  ];
+}
